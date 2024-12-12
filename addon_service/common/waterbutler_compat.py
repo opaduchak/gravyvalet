769e456bcff5e208e9dae6385c534e0fe0e58696
Oleh Paduchak <opaduchak@exoft.net>
2024-12-12 15:27:13 +0200

    fixed caching for wb_settings

--- addon_service/common/waterbutler_compat.py
+++ addon_service/common/waterbutler_compat.py
@@ -1,7 +1,4 @@
-import functools
-
 from asgiref.sync import async_to_sync
-from rest_framework_json_api import serializers
 
 from addon_service.addon_imp.instantiation import get_storage_addon_instance__blocking
 from addon_service.configured_addon.storage.models import ConfiguredStorageAddon
@@ -11,53 +8,43 @@ from addon_toolkit import (
 )
 
 
-class WaterButlerConfigSerializer(serializers.Serializer):
-    """Serialize ConfiguredStorageAddon information required by WaterButler.
+def build_waterbutler_config(configured_storage_addon: ConfiguredStorageAddon) -> dict:
+    wb_config = _build_config(configured_storage_addon)
+    creds = _build_credentials(configured_storage_addon, wb_config)
+    return {
+        "config": wb_config,
+        "credentials": creds,
+    }
 
-    The returned data should share a shape with the existing `serialize_waterbutler_credentials`
-    and `serialize_waterbutler_settings` functions used by the OSF-based Addons.
-    """
 
-    class JSONAPIMeta:
-        resource_name = "waterbutler-config"
+def _build_config(configured_storage_addon: ConfiguredStorageAddon):
+    imp = get_storage_addon_instance__blocking(
+        configured_storage_addon.imp_cls,
+        configured_storage_addon.base_account,
+        configured_storage_addon.config,
+    )
+    wb_config = async_to_sync(imp.build_wb_config)()
+    return wb_config
 
-    credentials = serializers.SerializerMethodField("_credentials_for_waterbutler")
-    config = serializers.SerializerMethodField("_config_for_waterbutler")
 
-    def _credentials_for_waterbutler(self, configured_storage_addon):
-        _creds_data = configured_storage_addon.credentials
-        imp = get_storage_addon_instance__blocking(
-            configured_storage_addon.imp_cls,
-            configured_storage_addon.base_account,
-            configured_storage_addon.config,
-        )
-        wb_config = async_to_sync(imp.build_wb_config)()
-
-        match type(_creds_data):
-            case credentials.AccessTokenCredentials:
-                creds = {"token": _creds_data.access_token}
-                if "host" in wb_config:
-                    creds["host"] = wb_config["host"]
-                return creds
-            case (
-                credentials.AccessKeySecretKeyCredentials
-                | credentials.UsernamePasswordCredentials
-            ):
-                # field names line up with waterbutler's expectations
-                serialized_creds = json_arguments.json_for_dataclass(_creds_data)
-                if "host" in wb_config:
-                    serialized_creds["host"] = wb_config["host"]
-                return serialized_creds
-            case _:
-                raise ValueError(f"unknown credentials type: {_creds_data}")
-
-    @staticmethod
-    @functools.cache
-    def _config_for_waterbutler(configured_storage_addon: ConfiguredStorageAddon):
-        imp = get_storage_addon_instance__blocking(
-            configured_storage_addon.imp_cls,
-            configured_storage_addon.base_account,
-            configured_storage_addon.config,
-        )
-        wb_config = async_to_sync(imp.build_wb_config)()
-        return wb_config
+def _build_credentials(
+    configured_storage_addon: ConfiguredStorageAddon, wb_config: dict
+):
+    _creds_data = configured_storage_addon.credentials
+    match type(_creds_data):
+        case credentials.AccessTokenCredentials:
+            creds = {"token": _creds_data.access_token}
+            if "host" in wb_config:
+                creds["host"] = wb_config["host"]
+            return creds
+        case (
+            credentials.AccessKeySecretKeyCredentials
+            | credentials.UsernamePasswordCredentials
+        ):
+            # field names line up with waterbutler's expectations
+            serialized_creds = json_arguments.json_for_dataclass(_creds_data)
+            if "host" in wb_config:
+                serialized_creds["host"] = wb_config["host"]
+            return serialized_creds
+        case _:
+            raise ValueError(f"unknown credentials type: {_creds_data}")
